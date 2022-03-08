28aee426aed2e5cc49c49550b6bc6549416fd7cd
Oscar Szumiak <oscarszumiak@gmail.com>
2022-03-08 20:40:40 +0100

    Initial solution to ZadPrzekazywanieTablic.cc

--- kcppZadania/ZadPrzekazywanieTablic.cc
+++ kcppZadania/ZadPrzekazywanieTablic.cc
@@ -8,6 +8,28 @@ using namespace std;
  * Nazwać program: ZadPrzekazywanieTablic.cc
  */
 
+const int SIZE = 10;
+int seq_table[SIZE];
+
+void print_arr(int* table, int size) {
+    cout << "[";
+    for (int i=0; i<size-1; i++) {
+        cout << table[i] << ", ";
+    }
+    cout << table[size-1] << "]" << endl;
+}
+
+void fib_seq(int* table, int size) {
+    table[0] = 0;
+    table[1] = 1;
+    for (int i=2; i<size; i++) {
+        table[i] = table[i-2] + table[i-1];
+    }
+}
+
 int main() {
+    print_arr(seq_table, SIZE);
+    fib_seq(seq_table, SIZE);
+    print_arr(seq_table, SIZE);
     return 0;
 }
