e8caa77590ff807cf4ac5437bef804a9532f52b2
Oscar Szumiak <oscarszumiak@gmail.com>
2022-04-01 01:30:55 +0200

    Completion of task ZadZwracanie.cc

--- kcppZadania/ZadZwracanie.cc
+++ kcppZadania/ZadZwracanie.cc
@@ -12,19 +12,34 @@ using namespace std;
  * Nazwać program: ZadZwracanie.cc
  */
 
+int x;
+
 int fval(int a, int b) {
-    
+    return a + b;
 }
 
-&int fref(int a, int b) {
-
+int& fref(int a, int b) {
+    x = a + b;
+    return x;
 }
 
-*int fptr(int a, int b) {
-     
+int* fptr(int a, int b, int* p) {
+    *p = a + b;
+    return p;
+}
+
+void ftab(int a, int b, int tab[]) {
+    tab[0] = a + b;
 }
 
 int main() {
-    
+    cout << "Suma poprzez wartość: " << fval(1, 2) << endl;
+    cout << "Suma poprzez referencję: " << fref(1, 2) << endl;
+    int *p = new int;
+    cout << "Suma poprzez wskaźnik: " << *fptr(1, 2, p) << endl;
+    delete p;
+    int tablica[1];
+    ftab(1, 2, tablica);
+    cout << "Suma poprzez tablicę: " << tablica[0] << endl;
     return 0;
 }
